cbbad06569456cd4c97009da011694a71ffffd1e
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-01-26 10:08:43 -0800

    Added Animation to Orc: -added animation -collide tiles

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,14 +13,19 @@ game.PlayerEntity = me.Entity.extend({
 			spritewidth: "64",
 			/*sprite height tells us the height of the image*/
 			sprieheight: "64",
-
+			/*function creates new shape*/
 			getShape: function() {
 				/*returns new shape: rectangle*/ /*turns rectangle to polygon*/
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
 		/*sets speed 5 units to the right*/
-		this.body.setVelocity(5, 0);
+		this.body.setVelocity(5, 20);
+		this.renderable.addAnimation("idle", [78]);
+
+		this.renderable.addAnimation("walk", [117, 118,119,120, 121, 122, 123, 124, 125], 80);
+
+		this.renderable.setCurrentAnimation("idle");
 	},
 
 	update: function(delta){
@@ -29,17 +34,32 @@ game.PlayerEntity = me.Entity.extend({
 			/*adds to the position of x by the velocity defined above in setVelocity() and multiplying it by me.timer.tick
 			me.timer.tick makes the movement look smooth*/
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			/*flips player animation to opposite direction*/
+			this.flipX(true);
+			
 		}
 		/*checks if left key has been pressed*/
 		else if(me.input.isKeyPressed("left")) {
 			/*adds to the position of x by the velocity defined above in setVelocity() and multiplying it by me.timer.tick
 			me.timer.tick makes the movement look smooth*/
 			this.body.vel.x -= this.body.accel.x * me.timer.tick;
+			/*flips player animation to opposite direction from where it was flipped*/
+			this.flipX(false);
 		}
 		else{
+			/*brings to velocity down to 0 when key isnt pressed*/
 			this.body.vel.x = 0;
 		}
+		if (this.body.vel.x !== 0) {
+		if (!this.renderable.isCurrentAnimation("walk")) {
+			this.renderable.setCurrentAnimation("walk");
+		}
+	}
+	else{
+		this.renderable.setCurrentAnimation("idle");
+	}
 		this.body.update(delta);
+		this._super(me.Entity, "update", [delta]);
 		return true;
 	}
 });
