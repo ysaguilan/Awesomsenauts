61354ccf04734c8db203b14575ee80ece5d64507
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-02-24 12:50:47 -0800

    27 Making the Title Screen Functional: -Made title screen Appear -doesn't take the whole screen -Start button (enter) wont work

--- js/screens/title.js
+++ js/screens/title.js
@@ -7,15 +7,15 @@ game.TitleScreen = me.ScreenObject.extend({
 	me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10);
 	me.input.bindKey(me.input.KEY.ENTER, "start");
 
-	me.game.world.addChild(new(me.Renderable.extend({
+	me.game.world.addChild(new (me.Renderable.extend({
 
 		init: function() {
-			this._super(me.Renderable, 'init', 510, 30, me.game.viewport.height]);
+			this._super(me.Renderable, 'init', [510, 30,  me.game.viewport.width, me.game.viewport.height]);
 			this.font = new me.Font("Arial", 46, "white");
 		},
 
 		draw: function(renderer) {
-			this.font.draw(renderer.getContext(), "Awsomenauts", 450, 140);
+			this.font.draw(renderer.getContext(), "Awsomenauts", 450, 130);
 			this.font.draw(renderer.getContext(), "Press Enter", 250, 530);
 		},
 
@@ -23,8 +23,9 @@ game.TitleScreen = me.ScreenObject.extend({
 
 	this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
 		if (action === "start") {
-
+			me.state.change(me.state.PLAY);
 		}
+		});
 	},
 	
 	
@@ -32,6 +33,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		 // TODO
+		 me.input.unbindKey(me.input.KEY.ENTER);
+		 me.event.unsubcribe(this.handler);
 	}
 });
