79bd26c0f30d8c50e98725cc2671c0b37d7653bf
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-02-27 19:05:23 -0800

    33 Refactor Collision: - Refactored Player's Collisions

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -160,58 +160,11 @@ game.PlayerEntity = me.Entity.extend({
 
 	collideHandler: function(response) {
 		if (response.b.type ==='EnemyBaseEntity') {
-			var ydif = this.pos.y  - response.b.pos.y;
-			var xdif = this.pos.x - response.b.pos.x;
-
-			console.log("xdif" + xdif + " ydif " + ydif);
-			/*checks if player has made contact to the top ofr enemy base if it has then player wont go throught the top*/
-			if (ydif<-40 && xdif<70 && xdif>-35) {
-				this.body.falling = false;
-				this.body.vel.y = -1;
-			}
-			/*wont allow player go through the right side of enemy base*/
-			else if (xdif>-35 && this.facing==='right' && (xdif<0) && ydif>-50) {
-				this.body.vel.x = 0;
-				//this.pos.x = this.pos.x -1;
-			}
-			/*wont allow player go through the left side of enemy base*/
-			else if (xdif<70 && this.facing==='left' && (xdif>0)) {
-				this.body.vel.x = 0; 
-				//this.pos.x = this.pos.x +1;
-			}
-
-			if (this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit>= game.data.playerAttackTimer) {
-				console.log("tower Hit");
-				this.lastHit = this.now;
-				response.b.loseHealth(game.data.playerAttack);
-			}
+			this.collideWithEnemyBase(response);
+		}
 			else if(response.b.type === 'EnemyCreep'){
-				var xdif = this.pos.x - response.b.pos.x;
-				var ydif = this.pos.y + response.b.pos.y;
-
-				if (xdif>0) {
-				//	this.pos.x = this.pos.x + 1;
-					if (this.facing === "left") {
-						this.body.vel.x = 0;
-					}
-				}
-				else{
-				//	this.pos.x  = this.pos.x - 1;
-					if (this.facing === "right") {
-						this.body.vel.x = 0;
-					}
-				}
-
-				if (this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit>= game.data.playerAttackTimer && (Math.abs(ydif) <=40) && ((xdif>0) && this.facing === "left") || ((xdif<0) && this.facing === "right")) {
-					this.lastHit = this.now;
-					//if the creeps health is less than our attack excutes code in if statement
-					if (response.b.health <=this.attack) {
-						//adds one gold for a creep kill
-						game.data.gold += 1;
-						console.log("Current gold: " +game.data.gold);
-					}
-					response.b.loseHealth(game.data.playerAttack);
-				}
+				this.collideWithEnemyCreep(response);	
+			}
 
 				else if ( response.b.type === 'EnemyCreep') {
 					if(this.renderable.isCurrentAnimation("attack")) {
@@ -220,7 +173,74 @@ game.PlayerEntity = me.Entity.extend({
 				}
 			}
 		}
+	},
+
+collideWithEnemyBase: function(response) {
+	var ydif = this.pos.y  - response.b.pos.y;
+	var xdif = this.pos.x - response.b.pos.x;
+
+	console.log("xdif" + xdif + " ydif " + ydif);
+	/*checks if player has made contact to the top ofr enemy base if it has then player wont go throught the top*/
+	if (ydif<-40 && xdif<70 && xdif>-35) {
+		this.body.falling = false;
+		this.body.vel.y = -1;
 	}
+	/*wont allow player go through the right side of enemy base*/
+	else if (xdif>-35 && this.facing==='right' && (xdif<0) && ydif>-50) {
+		this.body.vel.x = 0;
+	}
+	/*wont allow player go through the left side of enemy base*/
+	else if (xdif<70 && this.facing==='left' && (xdif>0)) {
+		this.body.vel.x = 0; 
+	}
+
+	if (this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit>= game.data.playerAttackTimer) {
+		console.log("tower Hit");
+		this.lastHit = this.now;
+		response.b.loseHealth(game.data.playerAttack);
+	}
+},
+
+collideWithEnemyCreep: function() {
+	var xdif = this.pos.x - response.b.pos.x;
+	var ydif = this.pos.y + response.b.pos.y;
+
+	this.stopMovement(xdif);
+	if(this.checkAttack(xdif, ydif)) {
+		this.hitCreep(response);
+	},
+
+stopMovement: function(xdif) {
+	if (xdif>0) {
+		if (this.facing === "left") {
+			this.body.vel.x = 0;
+		}
+	}
+	else{
+		if (this.facing === "right") {
+		this.body.vel.x = 0;
+		}
+	}
+}, 
+
+checkAttack: function(xdif, ydif) {
+	if (this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit>= game.data.playerAttackTimer && (Math.abs(ydif) <=40) && ((xdif>0) && this.facing === "left") || ((xdif<0) && this.facing === "right")) {
+		this.lastHit = this.now;
+		return true;
+	}
+	return false;
+},
+
+hitCreep: function() {
+	//if the creeps health is less than our attack excutes code in if statement
+	if (response.b.health <=this.attack) {
+		//adds one gold for a creep kill
+		game.data.gold += 1;
+		console.log("Current gold: " +game.data.gold);
+	}
+	response.b.loseHealth(game.data.playerAttack);
+	}	
+}
 });
 
 game.PlayerBaseEntity = me.Entity.extend({
