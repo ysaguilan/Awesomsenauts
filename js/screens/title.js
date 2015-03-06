2a6604d3d97cc28d82ce8501a7b590bb75610a94
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-03-06 09:19:00 -0800

    39 Start New: -Added Start New Option - Able To Choose Start New Option By Clicking "Start New"

--- js/screens/title.js
+++ js/screens/title.js
@@ -5,27 +5,37 @@ game.TitleScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 
 	me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10);
-	me.input.bindKey(me.input.KEY.ENTER, "start");
+
 
 	me.game.world.addChild(new (me.Renderable.extend({
 
 		init: function() {
-			this._super(me.Renderable, 'init', [510, 30,  me.game.viewport.width, me.game.viewport.height]);
+			this._super(me.Renderable, 'init', [270, 240,  300, 50]);
 			this.font = new me.Font("Arial", 46, "white");
+			me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
 		},
 
 	draw: function(renderer) {
 			this.font.draw(renderer.getContext(), "Awsomenauts", 450, 130);
-			this.font.draw(renderer.getContext(), "Press Enter", 250, 530);
+			this.font.draw(renderer.getContext(), "Start A New Game", this.pos.x, this.pos.y);
 	},
+
+	update: function(dt) {
+		return true;
+	},
+
+	newGame: function() {
+		me.input.releasePointerEvent('pointerdown', this);
+		me.save.remove('exp');
+		me.save.remove('exp1');
+		me.save.remove('exp3');
+		me.save.remove('exp4');
+		me.state.change(me.state.PLAY);
+	}
 
 	})));
 
-	this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
-	if (action === "start") {
-			me.state.change(me.state.PLAY);
-			}
-		})
+
 	},
 	
 	
@@ -33,7 +43,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		 me.input.unbindKey(me.input.KEY.ENTER);
-		 me.event.unsubscribe(this.handler);
+
 	}
 });
