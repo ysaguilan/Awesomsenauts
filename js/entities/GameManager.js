0baa78d689249a837cad222ed38fdc9183daca9b
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-03-06 08:53:46 -0800

    38 Saving with MelonJS: -Added Saving With MelonJS -Save Experience

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -68,14 +68,23 @@ game.ExperienceManager = Object.extend({
 	},
 	update: function() {
 		if (game.data.win === true && !this.gameOver) {
-			game.data.exp += 10;
-			this.gameOver = true;
+			this.gameOver(win);
 		}
 		else if(game.data.win === false) {
-			game.data.exp += 1;
-			this.gameOver = true;
+			this.gameOver(false);
 		}
-		console.log(game.data.exp);
+	
 		return true;
+	},
+	gameOver: function(win) {
+		if (win) {
+			game.data.exp += 10;
+		}
+		else {
+			game.data.exp += 1;
+		}
+	
+		this.gameOver = true;
+		me.save.exp = game.data.exp;
 	}
 });
