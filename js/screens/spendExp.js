714e81836320ccbfc0b36f7441eafc2e6df1c579
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-03-12 08:56:04 -0700

    43 Adding Text : - Added Text to Spend Exp Screen -Text Displays Spending Options

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -10,13 +10,19 @@ game.SpendExp = me.ScreenObject.extend({
 	me.game.world.addChild(new (me.Renderable.extend({
 
 		init: function() {
-			this._super(me.Renderable, 'init', [270, 240,  300, 50]);
+			this._super(me.Renderable, 'init', [10, 10,  300, 50]);
 			this.font = new me.Font("Arial", 46, "white");
 		},
 
 	draw: function(renderer) {
 
-			this.font.draw(renderer.getContext(), "Spend", this.pos.x, this.pos.y);
+			this.font.draw(renderer.getContext(), "Press F1 - F4 To Spend, F5 To Skip", this.pos.x, this.pos.y);
+			this.font.draw(renderer.getContext(), "Current Exp: " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
+			this.font.draw(renderer.getContext(), "F1: Increase Gold Production" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 100);
+			this.font.draw(renderer.getContext(), "F2: Add Starting Gold" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 150);
+			this.font.draw(renderer.getContext(), "F3: Increase Attack Damage" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 200);
+			this.font.draw(renderer.getContext(), "F4: Increase Health" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 250);
+
 	},
 
 	})));
--- js/screens/title.js
+++ js/screens/title.js
@@ -4,7 +4,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {
 
-	me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10);
+	me.game.world.addChild(new me.Sprite(50, 50, me.loader.getImage('title-screen')), -10);
 
 
 	me.game.world.addChild(new (me.Renderable.extend({
@@ -30,6 +30,7 @@ game.TitleScreen = me.ScreenObject.extend({
 		me.save.remove('exp1');
 		me.save.remove('exp3');
 		me.save.remove('exp4');
+		me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
 		me.state.change(me.state.PLAY);
 	}
 
@@ -38,7 +39,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	me.game.world.addChild(new (me.Renderable.extend({
 
 		init: function() {
-			this._super(me.Renderable, 'init', [380, 340,  250, 50]);
+			this._super(me.Renderable, 'init', [380, 330,  250, 50]);
 			this.font = new me.Font("Arial", 46, "white");
 			me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
 		},
@@ -52,9 +53,15 @@ game.TitleScreen = me.ScreenObject.extend({
 	},
 
 	newGame: function() {
+		game.data.exp = me.save.exp;
+		game.data.exp1 = me.save.exp1;
+		game.data.exp2 = me.save.exp2;
+		game.data.exp3 = me.save.exp3;
+		game.data.exp4 = me.save.exp4;
+
 		me.input.releasePointerEvent('pointerdown', this);
 
-		me.state.change(me.state.PLAY);
+		me.state.change(me.state.SPENDEXP);
 	}
 
 	})));
