937b346320ffb9950bcb9866e3578744cf4aedcf
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-03-13 08:32:45 -0700

    44 Finishing SpendEXP Screen: - Finished Spend Exp Screen - Binded Keys F1 - F5 - Added Function To F5 Button To Skip Spend Exp Screen

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -6,26 +6,51 @@ game.SpendExp = me.ScreenObject.extend({
 
 	me.game.world.addChild(new me.Sprite(50, 50, me.loader.getImage('exp-screen')), -10);
 
+	me.input.bindKey(me.input.KEY.F1, "F1");
+	me.input.bindKey(me.input.KEY.F2, "F2");
+	me.input.bindKey(me.input.KEY.F3, "F3");
+	me.input.bindKey(me.input.KEY.F4, "F4");
+	me.input.bindKey(me.input.KEY.F5, "F5");
+	
+
 
 	me.game.world.addChild(new (me.Renderable.extend({
 
 		init: function() {
 			this._super(me.Renderable, 'init', [10, 10,  300, 50]);
-			this.font = new me.Font("Arial", 46, "white");
+			this.font = new me.Font("Arial", 26, "white");
 		},
-
+ 
 	draw: function(renderer) {
 
-			this.font.draw(renderer.getContext(), "Press F1 - F4 To Spend, F5 To Skip", this.pos.x, this.pos.y);
-			this.font.draw(renderer.getContext(), "Current Exp: " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
-			this.font.draw(renderer.getContext(), "F1: Increase Gold Production" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 100);
-			this.font.draw(renderer.getContext(), "F2: Add Starting Gold" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 150);
-			this.font.draw(renderer.getContext(), "F3: Increase Attack Damage" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 200);
-			this.font.draw(renderer.getContext(), "F4: Increase Health" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 250);
+		this.font.draw(renderer.getContext(), "Press F1 - F4 To Spend, F5 To Skip", this.pos.x, this.pos.y);
+		this.font.draw(renderer.getContext(), "Current Exp: " + game.data.exp.toString(), this.pos.x, this.pos.y + 50);
+		this.font.draw(renderer.getContext(), "F1: Increase Gold Production CURRENT LEVEL" + game.data.exp1.toString() + " COST " + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
+		this.font.draw(renderer.getContext(), "F2: Add Starting Gold", this.pos.x, this.pos.y + 150);
+		this.font.draw(renderer.getContext(), "F3: Increase Attack Damage", this.pos.x, this.pos.y + 200);
+		this.font.draw(renderer.getContext(), "F4: Increase Health", this.pos.x, this.pos.y + 250);
 
 	},
 
 	})));
+
+	this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
+		if (action === "F1") {
+
+		}
+		else if (action === "F2") {
+
+		}
+		else if (action === "F3") {
+
+		}
+		else if (action === "F4") {
+
+		}
+		else if (action === "F5") {
+			me.state.change(me.state.PLAY);
+		}
+	});
 
 
 	},
@@ -35,6 +60,12 @@ game.SpendExp = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
+	me.input.unbindKey(me.input.KEY.F1, "F1");
+	me.input.unbindKey(me.input.KEY.F2, "F2");
+	me.input.unbindKey(me.input.KEY.F3, "F3");
+	me.input.unbindKey(me.input.KEY.F4, "F4");
+	me.input.unbindKey(me.input.KEY.F5, "F5");
 
+	me.unsubscribe(this.handler);
 	}
 });
