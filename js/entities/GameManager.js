bf27b2dec6741661db8680404f10c88587216b19
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-03-13 18:59:23 -0700

    47 Drawing spendGold image: - Finished Drawing spendGold Image - Pauses Game when Buy Button(B) Is Pressed - Only Pauses For A Few Seconds Before Player And Creeps Begin To Move Again

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -96,12 +96,40 @@ game.SpendGold = Object.extend({
 		this.now = new Date().getTime();
 		/*keeps track of the last time a creep has been made*/
 		this.lastBuy = new Date().getTime();
-
 		this.paused = false;
 		/*updates for changes*/
 		this.alwaysUpdate = true;
+		this.updateWhenPaused = true;
+		this.buying = false;
 	},
 	update: function() {
+		this.now = new Date().getTime();
+		if (me.input.isKeyPressed("buy") && this.now - this.lastBuy >= 1000) {
+			this.lastBuy = this.now;
+			if (!this.buying) {
+				this.startBuying();
+			}
+			else {
+				this.stopBuying();
+			}
+		}
+
 		return true;
+	},
+	startBuying: function() {
+		this.buying = true;
+		me.state.pause(me.state.PLAY);
+		game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+		game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
+		game.data.buyscreen.updateWhenPaused = true;
+		game.data.buyscreen.setOpacity(0.8);
+		me.game.world.addChild(game.data.buyscreen, 34);
+		game.data.player.body.setVelocity(0, 0);
+	},
+	stopBuying: function() {
+		this.buying = false;
+		me.state.resume(me.state.PLAY);
+		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+		me.game.world.removeChild(game.data.buyscreen);
 	}
 });
